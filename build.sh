610725bcc65476b5f3b3591d3edfcba3d8b2b5b9
unotovive <tec.unotovive@gmail.com>
2021-10-13 00:21:39 +0900

    feat: vercel build optimizatrion

--- build.sh
+++ build.sh
@@ -2,37 +2,37 @@
 rm rf dist
 
 # 全部ビルド
-slidev build -o dist/advance/1 slides/advance/1.md
-slidev build -o dist/advance/2 slides/advance/2.md
-slidev build -o dist/advance/3 slides/advance/3.md
-slidev build -o dist/advance/4 slides/advance/4.md
-slidev build -o dist/advance/5 slides/advance/5.md
-slidev build -o dist/advance/6 slides/advance/6.md
-slidev build -o dist/advance/7 slides/advance/7.md
-slidev build -o dist/advance/8 slides/advance/8.md
-slidev build -o dist/advance/9 slides/advance/9.md
-slidev build -o dist/advance/10 slides/advance/10.md
-slidev build -o dist/advance/11 slides/advance/11.md
-slidev build -o dist/advance/12 slides/advance/12.md
-slidev build -o dist/advance/13 slides/advance/13.md
-slidev build -o dist/advance/14 slides/advance/14.md
-slidev build -o dist/advance/15 slides/advance/15.md
-slidev build -o dist/advance/16 slides/advance/16.md
+slidev build -o dist/advance/1 --base "/advance/1" slides/advance/1.md
+slidev build -o dist/advance/2 --base "/advance/2" slides/advance/2.md
+slidev build -o dist/advance/3 --base "/advance/3" slides/advance/3.md
+slidev build -o dist/advance/4 --base "/advance/4" slides/advance/4.md
+slidev build -o dist/advance/5 --base "/advance/5" slides/advance/5.md
+slidev build -o dist/advance/6 --base "/advance/6" slides/advance/6.md
+slidev build -o dist/advance/7 --base "/advance/7" slides/advance/7.md
+slidev build -o dist/advance/8 --base "/advance/8" slides/advance/8.md
+slidev build -o dist/advance/9 --base "/advance/9" slides/advance/9.md
+slidev build -o dist/advance/10 --base "/advance/10" slides/advance/10.md
+slidev build -o dist/advance/11 --base "/advance/11" slides/advance/11.md
+slidev build -o dist/advance/12 --base "/advance/12" slides/advance/12.md
+slidev build -o dist/advance/13 --base "/advance/13" slides/advance/13.md
+slidev build -o dist/advance/14 --base "/advance/14" slides/advance/14.md
+slidev build -o dist/advance/15 --base "/advance/15" slides/advance/15.md
+slidev build -o dist/advance/16 --base "/advance/16" slides/advance/16.md
 
 # 全部ビルド
-slidev build -o dist/biginner/1 slides/biginner/1.md
-slidev build -o dist/biginner/2 slides/biginner/2.md
-slidev build -o dist/biginner/3 slides/biginner/3.md
-slidev build -o dist/biginner/4 slides/biginner/4.md
-slidev build -o dist/biginner/5 slides/biginner/5.md
-slidev build -o dist/biginner/6 slides/biginner/6.md
-slidev build -o dist/biginner/7 slides/biginner/7.md
-slidev build -o dist/biginner/8 slides/biginner/8.md
-slidev build -o dist/biginner/9 slides/biginner/9.md
-slidev build -o dist/biginner/10 slides/biginner/10.md
-slidev build -o dist/biginner/11 slides/biginner/11.md
-slidev build -o dist/biginner/12 slides/biginner/12.md
-slidev build -o dist/biginner/13 slides/biginner/13.md
-slidev build -o dist/biginner/14 slides/biginner/14.md
-slidev build -o dist/biginner/15 slides/biginner/15.md
-slidev build -o dist/biginner/16 slides/biginner/16.md
+slidev build -o dist/biginner/1 --base "/biginner/1" slides/biginner/1.md
+slidev build -o dist/biginner/2 --base "/biginner/2" slides/biginner/2.md
+slidev build -o dist/biginner/3 --base "/biginner/3" slides/biginner/3.md
+slidev build -o dist/biginner/4 --base "/biginner/4" slides/biginner/4.md
+slidev build -o dist/biginner/5 --base "/biginner/5" slides/biginner/5.md
+slidev build -o dist/biginner/6 --base "/biginner/6" slides/biginner/6.md
+slidev build -o dist/biginner/7 --base "/biginner/7" slides/biginner/7.md
+slidev build -o dist/biginner/8 --base "/biginner/8" slides/biginner/8.md
+slidev build -o dist/biginner/9 --base "/biginner/9" slides/biginner/9.md
+slidev build -o dist/biginner/10 --base "/biginner/10" slides/biginner/10.md
+slidev build -o dist/biginner/11 --base "/biginner/11" slides/biginner/11.md
+slidev build -o dist/biginner/12 --base "/biginner/12" slides/biginner/12.md
+slidev build -o dist/biginner/13 --base "/biginner/13" slides/biginner/13.md
+slidev build -o dist/biginner/14 --base "/biginner/14" slides/biginner/14.md
+slidev build -o dist/biginner/15 --base "/biginner/15" slides/biginner/15.md
+slidev build -o dist/biginner/16 --base "/biginner/16" slides/biginner/16.md
