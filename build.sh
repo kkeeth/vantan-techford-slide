858eaadcf0646b3c9a0788772a47f2364010af76
unotovive <tec.unotovive@gmail.com>
2021-10-12 13:55:51 +0900

    feat: build check

--- build.sh
+++ build.sh
@@ -3,36 +3,36 @@ rm rf dist
 
 # 全部ビルド
 slidev build -o dist/advance/1 slides/advance/1.md
-# slidev build -o dist/advance/2 slides/advance/2.md
-# slidev build -o dist/advance/3 slides/advance/3.md
-# slidev build -o dist/advance/4 slides/advance/4.md
-# slidev build -o dist/advance/5 slides/advance/5.md
-# slidev build -o dist/advance/6 slides/advance/6.md
-# slidev build -o dist/advance/7 slides/advance/7.md
-# slidev build -o dist/advance/8 slides/advance/8.md
-# slidev build -o dist/advance/9 slides/advance/9.md
-# slidev build -o dist/advance/10 slides/advance/10.md
-# slidev build -o dist/advance/11 slides/advance/11.md
-# slidev build -o dist/advance/12 slides/advance/12.md
-# slidev build -o dist/advance/13 slides/advance/13.md
-# slidev build -o dist/advance/14 slides/advance/14.md
-# slidev build -o dist/advance/15 slides/advance/15.md
-# slidev build -o dist/advance/16 slides/advance/16.md
+slidev build -o dist/advance/2 slides/advance/2.md
+slidev build -o dist/advance/3 slides/advance/3.md
+slidev build -o dist/advance/4 slides/advance/4.md
+slidev build -o dist/advance/5 slides/advance/5.md
+slidev build -o dist/advance/6 slides/advance/6.md
+slidev build -o dist/advance/7 slides/advance/7.md
+slidev build -o dist/advance/8 slides/advance/8.md
+slidev build -o dist/advance/9 slides/advance/9.md
+slidev build -o dist/advance/10 slides/advance/10.md
+slidev build -o dist/advance/11 slides/advance/11.md
+slidev build -o dist/advance/12 slides/advance/12.md
+slidev build -o dist/advance/13 slides/advance/13.md
+slidev build -o dist/advance/14 slides/advance/14.md
+slidev build -o dist/advance/15 slides/advance/15.md
+slidev build -o dist/advance/16 slides/advance/16.md
 
-# # 全部ビルド
-# slidev build -o dist/biginner/1 slides/biginner/1.md
-# slidev build -o dist/biginner/2 slides/biginner/2.md
-# slidev build -o dist/biginner/3 slides/biginner/3.md
-# slidev build -o dist/biginner/4 slides/biginner/4.md
-# slidev build -o dist/biginner/5 slides/biginner/5.md
-# slidev build -o dist/biginner/6 slides/biginner/6.md
-# slidev build -o dist/biginner/7 slides/biginner/7.md
-# slidev build -o dist/biginner/8 slides/biginner/8.md
-# slidev build -o dist/biginner/9 slides/biginner/9.md
-# slidev build -o dist/biginner/10 slides/biginner/10.md
-# slidev build -o dist/biginner/11 slides/biginner/11.md
-# slidev build -o dist/biginner/12 slides/biginner/12.md
-# slidev build -o dist/biginner/13 slides/biginner/13.md
-# slidev build -o dist/biginner/14 slides/biginner/14.md
-# slidev build -o dist/biginner/15 slides/biginner/15.md
-# slidev build -o dist/biginner/16 slides/biginner/16.md
+# 全部ビルド
+slidev build -o dist/biginner/1 slides/biginner/1.md
+slidev build -o dist/biginner/2 slides/biginner/2.md
+slidev build -o dist/biginner/3 slides/biginner/3.md
+slidev build -o dist/biginner/4 slides/biginner/4.md
+slidev build -o dist/biginner/5 slides/biginner/5.md
+slidev build -o dist/biginner/6 slides/biginner/6.md
+slidev build -o dist/biginner/7 slides/biginner/7.md
+slidev build -o dist/biginner/8 slides/biginner/8.md
+slidev build -o dist/biginner/9 slides/biginner/9.md
+slidev build -o dist/biginner/10 slides/biginner/10.md
+slidev build -o dist/biginner/11 slides/biginner/11.md
+slidev build -o dist/biginner/12 slides/biginner/12.md
+slidev build -o dist/biginner/13 slides/biginner/13.md
+slidev build -o dist/biginner/14 slides/biginner/14.md
+slidev build -o dist/biginner/15 slides/biginner/15.md
+slidev build -o dist/biginner/16 slides/biginner/16.md
