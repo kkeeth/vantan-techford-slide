ace6a065b9def1c24d9808d0a471609789f3d07d
unotovive <tec.unotovive@gmail.com>
2021-10-14 00:23:02 +0900

    feat: build optimazzation for typo

--- build.sh
+++ build.sh
@@ -20,19 +20,19 @@ slidev build -o dist/advance/15 --base "/advance/15" slides/advance/15.md
 slidev build -o dist/advance/16 --base "/advance/16" slides/advance/16.md
 
 # 全部ビルド
-slidev build -o dist/biginner/1 --base "/biginner/1" slides/biginner/1.md
-slidev build -o dist/biginner/2 --base "/biginner/2" slides/biginner/2.md
-slidev build -o dist/biginner/3 --base "/biginner/3" slides/biginner/3.md
-slidev build -o dist/biginner/4 --base "/biginner/4" slides/biginner/4.md
-slidev build -o dist/biginner/5 --base "/biginner/5" slides/biginner/5.md
-slidev build -o dist/biginner/6 --base "/biginner/6" slides/biginner/6.md
-slidev build -o dist/biginner/7 --base "/biginner/7" slides/biginner/7.md
-slidev build -o dist/biginner/8 --base "/biginner/8" slides/biginner/8.md
-slidev build -o dist/biginner/9 --base "/biginner/9" slides/biginner/9.md
-slidev build -o dist/biginner/10 --base "/biginner/10" slides/biginner/10.md
-slidev build -o dist/biginner/11 --base "/biginner/11" slides/biginner/11.md
-slidev build -o dist/biginner/12 --base "/biginner/12" slides/biginner/12.md
-slidev build -o dist/biginner/13 --base "/biginner/13" slides/biginner/13.md
-slidev build -o dist/biginner/14 --base "/biginner/14" slides/biginner/14.md
-slidev build -o dist/biginner/15 --base "/biginner/15" slides/biginner/15.md
-slidev build -o dist/biginner/16 --base "/biginner/16" slides/biginner/16.md
+slidev build -o dist/beginner/1 --base "/beginner/1" slides/beginner/1.md
+slidev build -o dist/beginner/2 --base "/beginner/2" slides/beginner/2.md
+slidev build -o dist/beginner/3 --base "/beginner/3" slides/beginner/3.md
+slidev build -o dist/beginner/4 --base "/beginner/4" slides/beginner/4.md
+slidev build -o dist/beginner/5 --base "/beginner/5" slides/beginner/5.md
+slidev build -o dist/beginner/6 --base "/beginner/6" slides/beginner/6.md
+slidev build -o dist/beginner/7 --base "/beginner/7" slides/beginner/7.md
+slidev build -o dist/beginner/8 --base "/beginner/8" slides/beginner/8.md
+slidev build -o dist/beginner/9 --base "/beginner/9" slides/beginner/9.md
+slidev build -o dist/beginner/10 --base "/beginner/10" slides/beginner/10.md
+slidev build -o dist/beginner/11 --base "/beginner/11" slides/beginner/11.md
+slidev build -o dist/beginner/12 --base "/beginner/12" slides/beginner/12.md
+slidev build -o dist/beginner/13 --base "/beginner/13" slides/beginner/13.md
+slidev build -o dist/beginner/14 --base "/beginner/14" slides/beginner/14.md
+slidev build -o dist/beginner/15 --base "/beginner/15" slides/beginner/15.md
+slidev build -o dist/beginner/16 --base "/beginner/16" slides/beginner/16.md
