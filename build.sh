fe103f11e846c73129700826674b94699ecb6804
kkeeth <zensin0082@gmail.com>
2022-05-12 22:54:28 +0900

    chore: updated directory configure

--- build.sh
+++ build.sh
@@ -2,39 +2,21 @@
 rm rf dist
 
 # 全部ビルド
-slidev build -o dist/advance/1 --base "/advance/1" slides/advance/1.md
-slidev build -o dist/advance/2 --base "/advance/2" slides/advance/2.md
-slidev build -o dist/advance/3 --base "/advance/3" slides/advance/3.md
-slidev build -o dist/advance/4 --base "/advance/4" slides/advance/4.md
-slidev build -o dist/advance/5 --base "/advance/5" slides/advance/5.md
-slidev build -o dist/advance/6 --base "/advance/6" slides/advance/6.md
-slidev build -o dist/advance/7 --base "/advance/7" slides/advance/7.md
-slidev build -o dist/advance/8 --base "/advance/8" slides/advance/8.md
-slidev build -o dist/advance/9 --base "/advance/9" slides/advance/9.md
-slidev build -o dist/advance/10 --base "/advance/10" slides/advance/10.md
-slidev build -o dist/advance/11 --base "/advance/11" slides/advance/11.md
-slidev build -o dist/advance/12 --base "/advance/12" slides/advance/12.md
-slidev build -o dist/advance/13 --base "/advance/13" slides/advance/13.md
-slidev build -o dist/advance/14 --base "/advance/14" slides/advance/14.md
-slidev build -o dist/advance/15 --base "/advance/15" slides/advance/15.md
-slidev build -o dist/advance/16 --base "/advance/16" slides/advance/16.md
-
-# 全部ビルド
-slidev build -o dist/beginner/1 --base "/beginner/1" slides/beginner/1.md
-slidev build -o dist/beginner/2 --base "/beginner/2" slides/beginner/2.md
-slidev build -o dist/beginner/3 --base "/beginner/3" slides/beginner/3.md
-slidev build -o dist/beginner/4 --base "/beginner/4" slides/beginner/4.md
-slidev build -o dist/beginner/5 --base "/beginner/5" slides/beginner/5.md
-slidev build -o dist/beginner/6 --base "/beginner/6" slides/beginner/6.md
-slidev build -o dist/beginner/7 --base "/beginner/7" slides/beginner/7.md
-slidev build -o dist/beginner/8 --base "/beginner/8" slides/beginner/8.md
-slidev build -o dist/beginner/9 --base "/beginner/9" slides/beginner/9.md
-slidev build -o dist/beginner/10 --base "/beginner/10" slides/beginner/10.md
-slidev build -o dist/beginner/11 --base "/beginner/11" slides/beginner/11.md
-slidev build -o dist/beginner/12 --base "/beginner/12" slides/beginner/12.md
-slidev build -o dist/beginner/13 --base "/beginner/13" slides/beginner/13.md
-slidev build -o dist/beginner/14 --base "/beginner/14" slides/beginner/14.md
-slidev build -o dist/beginner/15 --base "/beginner/15" slides/beginner/15.md
-slidev build -o dist/beginner/16 --base "/beginner/16" slides/beginner/16.md
+slidev build -o dist/1 --base "/1" slides/1.md
+slidev build -o dist/2 --base "/2" slides/2.md
+slidev build -o dist/3 --base "/3" slides/3.md
+slidev build -o dist/4 --base "/4" slides/4.md
+slidev build -o dist/5 --base "/5" slides/5.md
+slidev build -o dist/6 --base "/6" slides/6.md
+slidev build -o dist/7 --base "/7" slides/7.md
+slidev build -o dist/8 --base "/8" slides/8.md
+slidev build -o dist/9 --base "/9" slides/9.md
+slidev build -o dist/10 --base "/10" slides/10.md
+slidev build -o dist/11 --base "/11" slides/11.md
+slidev build -o dist/12 --base "/12" slides/12.md
+slidev build -o dist/13 --base "/13" slides/13.md
+slidev build -o dist/14 --base "/14" slides/14.md
+slidev build -o dist/15 --base "/15" slides/15.md
+slidev build -o dist/16 --base "/16" slides/16.md
 
 cp index.html dist/index.html
