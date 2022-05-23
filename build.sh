324f5e17f5387a501f319aa167255c8cd5b196c4
kkeeth <zensin0082@gmail.com>
2022-05-23 12:08:30 +0900

    feat: added base of 3.md

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm rf dist
 # 全部ビルド
 slidev build -o dist/1 --base "/1" slides/1.md
 slidev build -o dist/2 --base "/2" slides/2.md
-#slidev build -o dist/3 --base "/3" slides/3.md
+slidev build -o dist/3 --base "/3" slides/3.md
 #slidev build -o dist/4 --base "/4" slides/4.md
 #slidev build -o dist/5 --base "/5" slides/5.md
 #slidev build -o dist/6 --base "/6" slides/6.md
