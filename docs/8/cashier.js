fd5dcb7e5936cdb64932ed9a6513bcf30b0633ad
kkeeth <zensin0082@gmail.com>
2022-07-15 12:44:02 +0900

    fix: tweak homework answers

--- docs/8/cashier.js
+++ docs/8/cashier.js
@@ -7,12 +7,15 @@ const bagPrice = 5;
 
 // 3つのアイテムの合計を求める
 let okaikei = Number(item1.price) + Number(item2.price) + Number(item3.price);
+// 以下でも OK
+// let okaikei = (+item1.price) + (+item2.price) + (+item3.price);
 
 // 袋代を足す
 okaikei += bagPrice;
 
 // 消費税を計算する（軽減税率で8パー）
-const taxPrice = okaikei * 1.08;
+// 税を計算するので今回は外税
+okaikei *= 1.08;
 
 // 1000円超えてたらキャンペーンで10パーセントオフ
 // if文はまた今度やるけど細かいことは気にしないでください
