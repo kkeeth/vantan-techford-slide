7b085862d6a46a408bd28b41932a5ac3ea6e3e2d
kkeeth <zensin0082@gmail.com>
2022-07-15 12:11:21 +0900

    feat: answer about homework 2

---
+++ docs/8/cashier.js
@@ -0,0 +1,23 @@
+const item1 = { name: 'Apple', price: '200' }
+const item2 = { name: 'Orange', price: '100' }
+const item3 = { name: 'Lemon', price: '800' }
+
+//レジ袋の値段を定義（5円）
+const bagPrice = 5;
+
+// 3つのアイテムの合計を求める
+let okaikei = Number(item1.price) + Number(item2.price) + Number(item3.price);
+
+// 袋代を足す
+okaikei += bagPrice;
+
+// 消費税を計算する（軽減税率で8パー）
+const taxPrice = okaikei * 1.08;
+
+// 1000円超えてたらキャンペーンで10パーセントオフ
+// if文はまた今度やるけど細かいことは気にしないでください
+if (okaikei >= 1000) {
+  okaikei *= 0.9;
+}
+
+console.log(okaikei);
