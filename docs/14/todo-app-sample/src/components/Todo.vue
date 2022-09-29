8817cf333cd4fe46f6668d5676c32674f1d63338
kkeeth <zensin0082@gmail.com>
2022-09-30 03:00:59 +0900

    feat: empty todo page

---
+++ docs/14/todo-app-sample/src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo">todo component</div>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
--- docs/14/todo-app-sample/src/views/TodoView.vue
+++ docs/14/todo-app-sample/src/views/TodoView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="todo">
     <h1>This is an TODO page</h1>
+    <todo />
   </div>
 </template>
+
+<script>
+import Todo from "@/components/Todo.vue";
+
+export default {
+  name: "TodoView",
+  components: {
+    Todo,
+  },
+};
+</script>
