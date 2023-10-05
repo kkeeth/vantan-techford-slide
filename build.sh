b6a02d6e80cc5fb7c2abcd6e277d538fffddf501
kkeeth <zensin0082@gmail.com>
2023-10-06 08:28:09 +0900

    fix: 1.md build error

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
+# 全スライドのベースにならないように回避
+mv index.html top.html
+
 # 全部ビルド
-slidev build -o dist/1 --base /1/ slides/1.md
-slidev build -o dist/2 --base /2/ slides/2.md
-slidev build -o dist/3 --base /3/ slides/3.md
-slidev build -o dist/4 --base /4/ slides/4.md
-slidev build -o dist/5 --base /5/ slides/5.md
-slidev build -o dist/6 --base /6/ slides/6.md
-slidev build -o dist/7 --base /7/ slides/7.md
-slidev build -o dist/8 --base /8/ slides/8.md
-slidev build -o dist/9 --base /9/ slides/9.md
-slidev build -o dist/10 --base /10/ slides/10.md
-slidev build -o dist/11 --base /11/ slides/11.md
-slidev build -o dist/12 --base /12/ slides/12.md
-slidev build -o dist/13 --base /13/ slides/13.md
-slidev build -o dist/14 --base /14/ slides/14.md
+cp slides/1.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
 # slidev build -o dist/15 --base /15/ slides/15.md
 
-cp index.html dist/index.html
+# TOP ページ用
+cp top.html dist/index.html
 cp -R styles dist
+mv top.html index.html
