cad7f67a6cf69c6276f8ce3acf2e77ba9b2225d1
kkeeth <zensin0082@gmail.com>
2023-10-13 13:53:58 +0900

    urgent

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 mv index.html top.html
 
 # 全部ビルド
-# cp slides/1.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
-cp slides/2.md ./ && slidev build -o dist/2 --base /1/ 2.md && rm 2.md
+cp slides/2.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
+# cp slides/2.md ./ && slidev build -o dist/2 --base /1/ 2.md && rm 2.md
 # slidev build -o dist/15 --base /15/ slides/15.md
 
 # TOP ページ用
