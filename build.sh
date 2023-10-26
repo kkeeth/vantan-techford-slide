609a01ec543529de648864dd4e3f5925073941d0
kkeeth <zensin0082@gmail.com>
2023-10-26 22:37:29 +0900

    feat: wip 4.md

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mv index.html top.html
 cp slides/1.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
 cp slides/2.md ./ && slidev build -o dist/2 --base /2/ 2.md && rm 2.md
 cp slides/3.md ./ && slidev build -o dist/3 --base /3/ 3.md && rm 3.md
+cp slides/4.md ./ && slidev build -o dist/4 --base /4/ 4.md && rm 4.md
 # slidev build -o dist/15 --base /15/ slides/15.md
 
 # TOP ページ用
