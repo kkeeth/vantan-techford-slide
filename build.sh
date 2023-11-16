c39b013ec27f4fa2a14f74065cc6aa1ccfa1f49d
kkeeth <zensin0082@gmail.com>
2023-11-17 01:41:04 +0900

    wip: 50% completed 6.md

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cp slides/2.md ./ && slidev build -o dist/2 --base /2/ 2.md && rm 2.md
 cp slides/3.md ./ && slidev build -o dist/3 --base /3/ 3.md && rm 3.md
 cp slides/4.md ./ && slidev build -o dist/4 --base /4/ 4.md && rm 4.md
 cp slides/5.md ./ && slidev build -o dist/5 --base /5/ 5.md && rm 5.md
+cp slides/6.md ./ && slidev build -o dist/6 --base /6/ 6.md && rm 6.md
 # slidev build -o dist/15 --base /15/ slides/15.md
 
 # TOP ページ用
