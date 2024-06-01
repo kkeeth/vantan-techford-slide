c43b462531b939efdeda52bbf8d89054e25059c0
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-06-01 12:27:14 +0900

    feat: build about 5

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cp slides/1.md ./ && slidev build -o dist/1 --base /1/ 1.md && rm 1.md
 cp slides/2.md ./ && slidev build -o dist/2 --base /2/ 2.md && rm 2.md
 cp slides/3.md ./ && slidev build -o dist/3 --base /3/ 3.md && rm 3.md
 cp slides/4.md ./ && slidev build -o dist/4 --base /4/ 4.md && rm 4.md
-# cp slides/5.md ./ && slidev build -o dist/5 --base /5/ 5.md && rm 5.md
+cp slides/5.md ./ && slidev build -o dist/5 --base /5/ 5.md && rm 5.md
 # cp slides/6.md ./ && slidev build -o dist/6 --base /6/ 6.md && rm 6.md
 # cp slides/7.md ./ && slidev build -o dist/7 --base /7/ 7.md && rm 7.md
 # cp slides/8.md ./ && slidev build -o dist/8 --base /8/ 8.md && rm 8.md
