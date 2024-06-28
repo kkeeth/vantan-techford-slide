b22489c9ba11fcd2de83acfebbefbc0c630adbe5
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-06-28 23:05:19 +0900

    wip: 30% completed 8.md

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cp slides/4.md ./ && slidev build -o dist/4 --base /4/ 4.md && rm 4.md
 cp slides/5.md ./ && slidev build -o dist/5 --base /5/ 5.md && rm 5.md
 # cp slides/6.md ./ && slidev build -o dist/6 --base /6/ 6.md && rm 6.md
 cp slides/7.md ./ && slidev build -o dist/7 --base /7/ 7.md && rm 7.md
-# cp slides/8.md ./ && slidev build -o dist/8 --base /8/ 8.md && rm 8.md
+cp slides/8.md ./ && slidev build -o dist/8 --base /8/ 8.md && rm 8.md
 # cp slides/9.md ./ && slidev build -o dist/9 --base /9/ 9.md && rm 9.md
 # cp slides/10.md ./ && slidev build -o dist/10 --base /10/ 10.md && rm 10.md
 # cp slides/11.md ./ && slidev build -o dist/11 --base /11/ 11.md && rm 11.md
--- slides.md
+++ slides.md
@@ -3,7 +3,7 @@
 theme: seriph
 # random image from a curated Unsplash collection by Anthony
 # like them? see https://unsplash.com/collections/94734566/slidev
-background: https://source.unsplash.com/collection/94734566/1920x1080
+background: https://picsum.photos/1920/1080
 # apply any windi css classes to the current slide
 class: 'text-center'
 # https://sli.dev/custom/highlighters.html
@@ -108,7 +108,7 @@ Hover on the bottom-left corner to see the navigation's controls panel, [learn m
 
 ---
 layout: image-right
-image: https://source.unsplash.com/collection/94734566/1920x1080
+image: https://picsum.photos/1920/1080
 ---
 
 # Code
