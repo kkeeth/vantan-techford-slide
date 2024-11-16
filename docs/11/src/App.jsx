8b325de62fec710e9a486efca46d4bdbb558f8ed
kkeeth <zensin0082@gmail.com>
2024-11-16 15:04:14 +0900

    wip: slide 18

--- docs/11/src/App.jsx
+++ docs/11/src/App.jsx
@@ -1,89 +1,99 @@
-import { useState } from 'react'
-import './App.css'
-import AddTask from './components/AddTask'
-import TaskList from './components/TaskList'
-import Filter from './components/Filter'
+import { useState, useEffect } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import Filter from "./components/Filter";
 
 const initialList = [
-  {
-    id: 0,
-    title: 'aaaaa',
-    isDone: false,
-  },
-  {
-    id: 1,
-    title: 'bbbbb',
-    isDone: true,
-  },
-  {
-    id: 2,
-    title: 'ccccc',
-    isDone: false,
-  },
-]
+	{
+		id: 0,
+		title: "aaaaa",
+		isDone: false,
+	},
+	{
+		id: 1,
+		title: "bbbbb",
+		isDone: true,
+	},
+	{
+		id: 2,
+		title: "ccccc",
+		isDone: false,
+	},
+];
 
 const App = () => {
-  const [inputTask, setInputTask] = useState('')
-  const [taskList, setTaskList] = useState(initialList)
-  const [id, setId] = useState(initialList[initialList.length - 1].id)
-  const [filter, setFilter] = useState('ALL')
+	const [inputTask, setInputTask] = useState("");
+	const [taskList, setTaskList] = useState(initialList);
+	const [id, setId] = useState(initialList[initialList.length - 1].id);
+	const [filter, setFilter] = useState("ALL");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (inputTask === '') return
-    const newTaskList = [
-      ...taskList,
-      { id: id + 1, title: inputTask, isDone: false },
-    ]
-    setTaskList(newTaskList)
-    setId(id + 1)
-    setInputTask('')
-  }
+	useEffect(() => {
+		const items = JSON.parse(localStorage.getItem("taskList"));
+		console.log(items);
+		if (items) {
+			setTaskList(items);
+		}
+	}, []);
 
-  const handleTaskChange = (index) => {
-    const newTaskList = taskList.map((task) => {
-      if (task.id === index) {
-        task.isDone = !task.isDone
-      }
-      return task
-    })
-    setTaskList(newTaskList)
-    setInputTask('')
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (inputTask === "") return;
+		const newTaskList = [
+			...taskList,
+			{ id: id + 1, title: inputTask, isDone: false },
+		];
+		updateTasks(newTaskList);
+		setId(id + 1);
+		setInputTask("");
+	};
 
-  const handleAllRemoveTask = (updatedTasks) => {
-    setTaskList(updatedTasks)
-  }
+	const handleTaskChange = (index) => {
+		const newTaskList = taskList.map((task) => {
+			if (task.id === index) {
+				task.isDone = !task.isDone;
+			}
+			return task;
+		});
+		updateTasks(newTaskList);
+		setInputTask("");
+	};
 
-  const handleRemoveTask = (id) => {
-    const newTaskList = taskList.filter((task) => task.id !== id)
-    setTaskList(newTaskList)
-  }
+	const handleAllRemoveTask = (updatedTasks) => {
+		updateTasks(updatedTasks);
+	};
 
-  return (
-    <>
-      <div className="todo">
-        <h1>Todo List</h1>
-        <AddTask
-          inputTask={inputTask}
-          setInputTask={setInputTask}
-          handleSubmit={handleSubmit}
-        />
-        <hr />
-        <Filter
-          onChange={setFilter}
-          value={filter}
-        />
-        <TaskList
-          filter={filter}
-          taskList={taskList}
-          handleTaskChange={handleTaskChange}
-          handleRemoveTask={handleRemoveTask}
-          handleAllRemoveTask={handleAllRemoveTask}
-        />
-      </div>
-    </>
-  )
-}
+	const handleRemoveTask = (id) => {
+		const newTaskList = taskList.filter((task) => task.id !== id);
+		updateTasks(newTaskList);
+	};
 
-export default App
+	const updateTasks = (newTaskList) => {
+		localStorage.setItem("taskList", JSON.stringify(newTaskList));
+		setTaskList(newTaskList);
+	};
+
+	return (
+		<>
+			<div className="todo">
+				<h1>Todo List</h1>
+				<AddTask
+					inputTask={inputTask}
+					setInputTask={setInputTask}
+					handleSubmit={handleSubmit}
+				/>
+				<hr />
+				<Filter onChange={setFilter} value={filter} />
+				<TaskList
+					filter={filter}
+					taskList={taskList}
+					handleTaskChange={handleTaskChange}
+					handleRemoveTask={handleRemoveTask}
+					handleAllRemoveTask={handleAllRemoveTask}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default App;
