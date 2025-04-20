a9ba99a094f150533bf166abfac25706c5f374ff
kkeeth <zensin0082@gmail.com>
2025-04-20 22:28:08 +0900

    fix: link broken

--- assets/poke-search/vite.config.js
+++ assets/poke-search/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vantan-techford-slide/20/dist/',
+  base: '/',
 });
