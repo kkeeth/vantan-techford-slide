5dcc8c3355a24f2cbbd644bdf930b0d8dc0b5266
kkeeth <zensin0082@gmail.com>
2025-08-30 18:06:39 +0900

    refactor: formatting

--- assets/todo-app/src/components/Filter.tsx
+++ assets/todo-app/src/components/Filter.tsx
@@ -1,43 +1,50 @@
-import "./Filter.css";
+import './Filter.css';
 import { FilterType } from '../types';
 
 type FilterProps = {
-  value: FilterType
-  onChange: (filter: FilterType) => void
-}
+  value: FilterType;
+  onChange: (filter: FilterType) => void;
+};
 
 const Filter = ({ value, onChange }: FilterProps) => {
-	const handleClick = (key: FilterType) => {
-	  onChange(key)
-	}
+  const handleClick = (key: FilterType) => {
+    onChange(key);
+  };
 
-	return (
-		<>
-			<div className="todo-filter">
-				<input
-					type="radio"
-					name="filter" defaultChecked
-					className={value === "ALL" ? "is-active" : ""}
-				  onClick={() => handleClick("ALL")}
-				/>
-				<label>All</label>
-				<input
-          type="radio"
-          name="filter"
-          className={value === "TODO" ? "is-active" : ""}
-          onClick={() => handleClick("TODO")}
-        />
-				<label>ToDo</label>
-				<input
-          type="radio"
-          name="filter"
-          className={value === "DONE" ? "is-active" : ""}
-          onClick={() => handleClick("DONE")}
-        />
-				<label>Done</label>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="todo-filter">
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            defaultChecked
+            className={value === 'ALL' ? 'is-active' : ''}
+            onClick={() => handleClick('ALL')}
+          />
+          All
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            className={value === 'TODO' ? 'is-active' : ''}
+            onClick={() => handleClick('TODO')}
+          />
+          ToDo
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            className={value === 'DONE' ? 'is-active' : ''}
+            onClick={() => handleClick('DONE')}
+          />
+          Done
+        </label>
+      </div>
+    </>
+  );
 };
 
 export default Filter;
