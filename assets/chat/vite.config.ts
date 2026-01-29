f23c40f89512e7d5d29c1068f5821393deca9912
kkeeth <zensin0082@gmail.com>
2026-01-30 01:03:05 +0900

    fix: doesn't show app in production

--- assets/chat/vite.config.ts
+++ assets/chat/vite.config.ts
@@ -1,10 +1,10 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   test: {
     globals: true,
     environment: 'jsdom',
